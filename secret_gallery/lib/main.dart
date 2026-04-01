0a2f5cf2760c01337ec607b372f0d688e0feeadd
wwwalohacampus <h850415@naver.com>
2026-04-01 17:55:13 +0900

    시크릿갤러리앱 - 2 ✨

--- secret_gallery/lib/main.dart
+++ secret_gallery/lib/main.dart
@@ -1,4 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:secret_gallery/pages/album_list_page.dart';
+import 'package:secret_gallery/pages/gallery_picker_page.dart';
+import 'package:secret_gallery/pages/lock_page.dart';
+import 'package:secret_gallery/pages/photo_detail_page.dart';
+import 'package:secret_gallery/pages/photo_list_page.dart';
+import 'package:secret_gallery/pages/settings_page.dart';
 
 void main() {
   runApp(const MainApp());
@@ -9,12 +15,19 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
-      home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
-        ),
-      ),
+    return MaterialApp(
+      title: '시크릿 갤러리',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData.dark(),
+      initialRoute: '/lock',
+      routes: {
+        '/lock' : (context) => LockPage(),              // 잠금화면
+        '/album' : (context) => AlbumListPage(),        // 앨범목록
+        '/photo' : (context) => PhotoListPage(),        // 사진목록
+        '/picker' : (context) => GalleryPickerPage(),   // 사진선택
+        '/detail' : (context) => PhotoDetailPage(),     // 사진상세
+        '/setting' : (context) => SettingsPage(),       // 설정화면
+      },
     );
   }
 }
